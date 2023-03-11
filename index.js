4cd00c03747a943e7f77bb468711423f6705f1d4
Me <getdaniel>
2023-03-11 22:13:33 +0300

    configure contact us form

---
+++ index.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDrL_A052MByKBsPO7M4oJ-WPf1QIf1ASA",
+    authDomain: "my-profile-371308.firebaseapp.com",
+    databaseURL: "https://my-profile-371308-default-rtdb.firebaseio.com",
+    projectId: "my-profile-371308",
+    storageBucket: "my-profile-371308.appspot.com",
+    messagingSenderId: "42986837237",
+    appId: "1:42986837237:web:d50c3bb7a50631da2a61bc",
+    measurementId: "G-S535EY51Y3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+// const app = initializeApp(firebaseConfig);
