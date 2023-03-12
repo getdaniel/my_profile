5315bb403831513066830ae1253d4162a26d6201
Me <getdaniel>
2023-03-12 23:02:21 +0300

    add login auth

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDrL_A052MByKBsPO7M4oJ-WPf1QIf1ASA",
-    authDomain: "my-profile-371308.firebaseapp.com",
-    databaseURL: "https://my-profile-371308-default-rtdb.firebaseio.com",
-    projectId: "my-profile-371308",
-    storageBucket: "my-profile-371308.appspot.com",
-    messagingSenderId: "42986837237",
-    appId: "1:42986837237:web:d50c3bb7a50631da2a61bc",
-    measurementId: "G-S535EY51Y3"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-// const app = initializeApp(firebaseConfig);
-
 // Get a reference to the Firebase Realtime Database
 var database = firebase.database();
 
