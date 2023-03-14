164d49c22c01265846d01e05264a4daaef095aa7
Me <getdaniel>
2023-03-15 00:29:21 +0300

    reply and delete button

--- main/admin/retrieve_msg.js
+++ main/admin/retrieve_msg.js
@@ -29,7 +29,66 @@ messagesRef.on('child_added', snapshot => {
   messageCell.textContent = message.message;
   tableRow.appendChild(messageCell);
 
+  // Create a cell for the reply button
+  const replyButtonCell = document.createElement('td');
+  const replyButton = document.createElement('button');
+  replyButton.textContent = 'Reply';
+  replyButton.classList.add('btn', 'btn-primary');
+  replyButtonCell.appendChild(replyButton);
+  tableRow.appendChild(replyButtonCell);
+
+  // Create a cell for the delete button
+  const deleteButtonCell = document.createElement('td');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('btn', 'btn-danger');
+  deleteButtonCell.appendChild(deleteButton);
+  tableRow.appendChild(deleteButtonCell);
+
   // Add the new row to the table
   const messagesTable = document.querySelector('#messages-table tbody');
   messagesTable.appendChild(tableRow);
+
+  // Add a click listener to the reply button
+  replyButton.addEventListener('click', () => {
+    // Get the email address of the person who sent the message
+    const toEmail = message.email;
+
+    // Open the reply modal
+    const replyModal = document.querySelector('#reply-modal');
+    replyModal.style.display = 'block';
+
+    // Add a click listener to the send button in the reply modal
+    const sendButton = document.querySelector('#send-reply');
+    sendButton.addEventListener('click', () => {
+      // Get the subject and message of the reply
+      const subject = document.querySelector('#reply-subject').value;
+      const message = document.querySelector('#reply-message').value;
+
+      // Send the reply email
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://us-central1-your-project-id.cloudfunctions.net/sendEmail', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          alert('Reply sent!');
+          replyModal.style.display = 'none';
+        }
+      };
+      xhr.send(JSON.stringify({
+        toEmail: toEmail,
+        subject: subject,
+        message: message
+      }));
+    });
+  });
+
+  // Add a click listener to the delete button
+  deleteButton.addEventListener('click', () => {
+    // Remove the message from the database
+    messagesRef.child(snapshot.key).remove();
+
+    // Remove the message row from the table
+    messagesTable.removeChild(tableRow);
+  });
 });
