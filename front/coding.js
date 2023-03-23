7747018cb0edfd18a8776bb439398f8929956bb8
Me <getdaniel>
2023-03-23 14:02:08 +0300

    set value of db to frontend

--- front/coding.js
+++ front/coding.js
@@ -5,5 +5,10 @@ codingSkillsRef.on("value", function(snapshot) {
     var language = childSnapshot.val().language;
     var codingPercentage = childSnapshot.val().percentage;
     console.log("Language: " + language + ", Percentage: " + codingPercentage);
+
+    // Update the language and percentage value
+    document.querySelector(".progress-wrapper .caption").textContent = language;
+    document.querySelector(".progress-bar").style.width = codingPercentage + "%";
+    document.querySelector(".progress-bar").setAttribute("aria-valuenow", codingPercentage)
   });
 });
