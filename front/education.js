ab958e82c390d830ffe8484957743115f443df3d
Me <getdaniel>
2023-03-25 01:08:46 +0300

    education retrieve

--- front/education.js
+++ front/education.js
@@ -1,11 +1,41 @@
 const eduSkillsRef = firebase.database().ref("education");
 
 eduSkillsRef.on("value", function(snapshot) {
+  const eduTimeline = document.querySelector(".timeline");
+
   snapshot.forEach(function(childSnapshot) {
-    var eduDate = childSnapshot.val().date;
-    var eduCompany = childSnapshot.val().company;
-    var level = childSnapshot.val().level;
-    var eduDesc = childSnapshot.val().description;
-    console.log("Date: " + eduDate + ", Company: " + eduCompany + ", Level: " + level + ", Description: " + eduDesc);
+    const eduDate = childSnapshot.val().date;
+    const eduCompany = childSnapshot.val().company;
+    const level = childSnapshot.val().level;
+    const eduDesc = childSnapshot.val().description;
+
+    // Create a new timeline element for each education entry
+    const eduTimelineItem = document.createElement("li");
+
+    const eduTitle = document.createElement("div");
+    eduTitle.classList.add("title");
+    eduTitle.textContent = eduDate;
+
+    const eduDetails = document.createElement("div");
+    eduDetails.classList.add("details");
+
+    const eduHeader = document.createElement("h5");
+    eduHeader.textContent = eduCompany;
+
+    const eduSubheader = document.createElement("small");
+    eduSubheader.classList.add("fg-theme");
+    eduSubheader.textContent = level;
+
+    const eduDescription = document.createElement("p");
+    const eduCode = document.createElement("code");
+    eduCode.textContent = eduDesc;
+
+    eduDescription.appendChild(eduCode);
+    eduDetails.appendChild(eduHeader);
+    eduDetails.appendChild(eduSubheader);
+    eduDetails.appendChild(eduDescription);
+    eduTimelineItem.appendChild(eduTitle);
+    eduTimelineItem.appendChild(eduDetails);
+    eduTimeline.appendChild(eduTimelineItem);
   });
 });
